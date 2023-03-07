a2b832d1425e33a1a2172c40820cd4ed225ea504
leonhma <leonhardmasche@gmail.com>
2023-03-07 22:04:50 +0100

    forgot await

--- src/routes/api/get/+server.ts
+++ src/routes/api/get/+server.ts
@@ -2,13 +2,11 @@ import { hash_sha2 } from '$lib/auth';
 import { executeQuery } from '$lib/db';
 import { error, type RequestHandler } from '@sveltejs/kit';
 
-
-
 export const POST: RequestHandler = async ({ platform, cookies }) => {
 	if (!platform) throw error(501, 'No platform in worker');
 
 	let user = cookies.get('user');
-    let token = cookies.get('token');
+	let token = cookies.get('token');
 	if (!user || !token) {
 		user = crypto.randomUUID();
 		token = new TextDecoder().decode(crypto.getRandomValues(new Uint8Array(32)));
@@ -16,8 +14,8 @@ export const POST: RequestHandler = async ({ platform, cookies }) => {
 			platform,
 			'insert into users (user, token, number) values (?1, ?2, ?3)',
 			user,
-            hash_sha2(token),
-            0
+			await hash_sha2(token),
+			0
 		).then((res) => {
 			if (!res.success) throw error(502, 'Failed to insert user into database');
 		});
@@ -25,10 +23,15 @@ export const POST: RequestHandler = async ({ platform, cookies }) => {
 		cookies.set('token', token);
 	}
 
-    const { results } = await executeQuery(platform, 'select number from users where user = ?1 and token = ?2', user, hash_sha2(token));
-    console.log(results)
+	const { results } = await executeQuery(
+		platform,
+		'select number from users where user = ?1 and token = ?2',
+		user,
+		await hash_sha2(token)
+	);
+	console.log(results);
 
 	if (!results) throw error(404, 'No user in database');
 
-    return new Response(JSON.stringify({ number: results[0] }));
+	return new Response(JSON.stringify({ number: results[0] }));
 };
--- src/routes/api/set/+server.ts
+++ src/routes/api/set/+server.ts
@@ -3,11 +3,10 @@ import { executeQuery } from '$lib/db';
 import { error, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ platform, cookies, request }) => {
-	console.log(platform)
+	console.log(platform);
 	if (!platform) throw error(500, 'No platform in worker');
 	if (!cookies) throw error(404, 'No cookies present');
 	if (!request.body) throw error(404, 'No body in request');
-
 
 	const user = cookies.get('user');
 	const token = cookies.get('token');
@@ -17,7 +16,7 @@ export const POST: RequestHandler = async ({ platform, cookies, request }) => {
 		platform,
 		'update users set number = ?3 where user = ?1 and token = ?2',
 		user,
-		hash_sha2(token),
+		await hash_sha2(token),
 		parseInt(JSON.parse(request.body.getReader.toString()).number)
 	);
 
