89286fd50237fb45aa7fc9a4ed843f657fd1f26b
leonhma <leonhardmasche@gmail.com>
2023-03-07 22:35:14 +0100

    fixed hashing, response format

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,31 @@
+import { Buffer } from "buffer/"
+
+export function bufferToHex(buffer: ArrayBuffer): string {
+	return Array.prototype.map
+		.call(new Uint8Array(buffer), (b) => b.toString(16).padStart(2, '0'))
+		.join('');
+}
+
+export function hexToBuffer(hex: string): ArrayBuffer {
+	if (hex.length % 2 != 0) {
+		throw new TypeError('Expecting an even number of characters in the hexString');
+	}
+
+	const bad = hex.match(/[G-Z\s]{2}/gi);
+
+	if (bad) {
+		throw new TypeError(`Found non-hex characters [${bad.join()}]`);
+	}
+
+	const pairs = hex.match(/[\dA-F]{2}/gi);
+	const integers = pairs?.map((s) => parseInt(s, 16));
+	const array = new Uint8Array(integers ?? []);
+
+	return array.buffer;
+}
+
 export async function hash_sha2(text: string) {
-    return new TextDecoder().decode(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(text)));
+	return bufferToHex(
+		await crypto.subtle.digest('SHA-256', hexToBuffer(text))
+	);
 }
--- src/routes/api/get/+server.ts
+++ src/routes/api/get/+server.ts
@@ -1,15 +1,15 @@
-import { hash_sha2 } from '$lib/auth';
+import { bufferToHex, hash_sha2 } from '$lib/auth';
 import { executeQuery } from '$lib/db';
 import { error, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ platform, cookies }) => {
-	if (!platform) throw error(501, 'No platform in worker');
+    if (!platform) throw error(500, 'No platform in worker');
 
 	let user = cookies.get('user');
 	let token = cookies.get('token');
 	if (!user || !token) {
 		user = crypto.randomUUID();
-		token = new TextDecoder().decode(crypto.getRandomValues(new Uint8Array(32)));
+		token = bufferToHex(crypto.getRandomValues(new Uint8Array(32)).buffer);
 		await executeQuery(
 			platform,
 			'insert into users (user, token, number) values (?1, ?2, ?3)',
@@ -17,7 +17,7 @@ export const POST: RequestHandler = async ({ platform, cookies }) => {
 			await hash_sha2(token),
 			0
 		).then((res) => {
-			if (!res.success) throw error(502, 'Failed to insert user into database');
+			if (!res.success) throw error(500, 'Failed to insert user into database');
 		});
 		cookies.set('user', user);
 		cookies.set('token', token);
@@ -29,9 +29,8 @@ export const POST: RequestHandler = async ({ platform, cookies }) => {
 		user,
 		await hash_sha2(token)
 	);
-	console.log(results);
 
 	if (!results) throw error(404, 'No user in database');
 
-	return new Response(JSON.stringify({ number: results[0] }));
+	return new Response(JSON.stringify(results[0]));
 };
